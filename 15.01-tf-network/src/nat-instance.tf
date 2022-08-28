018d42e969e7376cdc93a53a950e294da7c40019
Aleksandr Belyaev <belas80@gmail.com>
2022-08-28 18:33:58 +0500

    15.01 all tasks completed

---
+++ 15.01-tf-network/src/nat-instance.tf
@@ -0,0 +1,30 @@
+resource "yandex_compute_instance" "nat-instance" {
+  name                      = "nat-instance"
+  platform_id               = "standard-v1"
+  zone                      = "ru-central1-a"
+  hostname                  = "nat-srv"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8josjq21d56924jfan"
+      size     = 30
+      type     = "network-ssd"
+    }
+  }
+
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.public.id
+    ip_address = "192.168.10.254"
+    nat        = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
